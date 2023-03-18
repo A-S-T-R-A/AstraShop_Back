faff2d5015c1d8fc6469691ce3d8ec49aa7646bb
Den4iK <gerasymenkoden@gmail.com>
2023-03-18 12:25:50 +0100

    start https

--- src/index.mjs
+++ src/index.mjs
@@ -7,12 +7,13 @@ import { getProductById } from "./controllers/product/get_product_by_id.mjs";
 import { getCategoryProducts } from "./controllers/category/get_category_products.mjs";
 import { getTopProducts } from "./controllers/product/get_top_products.mjs";
 import { getNewProducts } from "./controllers/product/get_new_products.mjs";
+import fs from "fs";
+import path from "path";
+import https from "https";
 
 const app = express();
 
 app.use(express.json());
-
-const database = Database();
 
 app.get("/api/v1/category/tree", getCategoryTree);
 app.get("/api/v1/category/:id/products", getCategoryProducts);
@@ -23,6 +24,14 @@ app.get("/api/v1/product/top", getTopProducts);
 app.get("/api/v1/product/new", getNewProducts);
 app.get("/api/v1/product/:id", getProductById);
 
-app.listen(3000, async () => {
-  database.start();
-});
+const options = {
+  ca: fs.readFileSync(path.resolve("cert", "ca_bundle.crt")),
+  key: fs.readFileSync(path.resolve("cert", "private.key")),
+  cert: fs.readFileSync(path.resolve("cert", "certificate.crt")),
+};
+
+https.createServer(options, app).listen(3000);
+
+const database = Database();
+
+database.start();
